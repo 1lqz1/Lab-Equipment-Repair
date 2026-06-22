794852fd43a16db0fc00ff3a0b7d4df33ef366f9
LiQingZ <li3608316@gmail.com>
2026-06-22 09:50:01 +0800

    配置后端数据库与MyBatis-Plus

--- backend/src/main/java/org/example/lab_equipment_repair/LabEquipmentRepairApplication.java
+++ backend/src/main/java/org/example/lab_equipment_repair/LabEquipmentRepairApplication.java
@@ -1,8 +1,10 @@
 package org.example.lab_equipment_repair;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("org.example.lab_equipment_repair.mapper")
 @SpringBootApplication
 public class LabEquipmentRepairApplication {
 
